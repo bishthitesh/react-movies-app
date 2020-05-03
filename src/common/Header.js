9c69a2b9aa26bc0b561b83102e76ad024f6ef9c4
Hitesh <hiteshbi@amazon.com>
2020-05-03 17:07:00 +0530

    Styled login form and added Login button inside it

--- src/common/Header.js
+++ src/common/Header.js
@@ -23,7 +23,7 @@ const customStyles = {
 
 const TabContainer = function (props) {
     return (
-        <Typography component="div" style={{ padding: 0 }}>
+        <Typography component="div" style={{padding: 0, textAlign: 'center'}}>
             {props.children}
         </Typography>
     )
@@ -48,7 +48,7 @@ class Header extends Component {
     }
 
     tabChangeHandler = (event, value) => {
-        this.setState({ value });
+        this.setState({value});
     }
 
     render() {
@@ -69,20 +69,22 @@ class Header extends Component {
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}
                 >
-                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
                         <Tab label="Login"/>
                         <Tab label="Register"/>
                     </Tabs>
                     <TabContainer>
                         <FormControl required>
                             <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input id="username" type="text" />
+                            <Input id="username" type="text"/>
                         </FormControl>
-
+                        <br/><br/>
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input id="password" type="password" />
+                            <Input id="password" type="password"/>
                         </FormControl>
+                        <br/><br/>
+                        <Button variant="contained" color="primary">LOGIN</Button>
                     </TabContainer>
                 </Modal>
             </div>
